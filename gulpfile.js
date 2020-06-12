c94161b5a2ee92a9eb8cdb1f67792e37e710f024
Clint Priest <cpriest@localhost>
2020-06-11 19:47:14 -0500

    .idea files w/ .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const MinecraftAddonBuilder = require('minecraft-addon-toolchain/v1');
+
+const builder = new MinecraftAddonBuilder('My Chunky Torch');
+
+module.exports = builder.configureEverythingForMe();
